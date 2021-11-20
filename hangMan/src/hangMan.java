edf82359c15bb3a1606424aff23ce392611377e5
mapioca <77710392+mapioca@users.noreply.github.com>
2021-11-20 13:23:49 -0700

    Added the functionality to select a random word form a word list

--- hangMan/src/hangMan.java
+++ hangMan/src/hangMan.java
@@ -1,17 +1,41 @@
 import java.io.*;
-import java.util.HashSet;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class hangMan {
-    static String DICTIONARY_NAME;
 
     public static void main(String[] args) throws FileNotFoundException {
-        Set<StringBuilder> dictionaryWords = new HashSet<>();
+        Map<Integer, StringBuilder> dictionaryWords = new HashMap<>();
+        String DICTIONARY_NAME = "words.txt";
+        StringBuilder randomWord = new StringBuilder();
+        boolean keepPlaying = true;
 
-        DICTIONARY_NAME = "words.txt";
         System.out.println("Hello user! Welcome to Hangman");
         dictionaryWords = getDictionaryWords(DICTIONARY_NAME);
+
+        //  Play until user wants to stop
+        while(keepPlaying) {
+            randomWord = getRandomWord(dictionaryWords);
+            playGame(randomWord);
+        }
+
+
+
+    }
+
+    private static void playGame(StringBuilder randomWord) {
+        System.out.println("The word to be guessed has " + randomWord.length() + " letter(s). Good luck!");
+
+    }
+
+    /**
+     * Gets a random word from the list
+     * @param dictionaryWords the list containing possible words to be guessed
+     * @return a random word to be guessed
+     */
+    private static StringBuilder getRandomWord(Map<Integer, StringBuilder> dictionaryWords) {
+        Random random = new Random();
+        int randomIndex = random.nextInt(dictionaryWords.size() - 1);
+        return dictionaryWords.get(randomIndex);
     }
 
     /**
@@ -20,15 +44,17 @@ public class hangMan {
      * @param name the name of the file
      * @return a set with words to guess
      */
-    public static Set<StringBuilder> getDictionaryWords(String name) throws FileNotFoundException {
-        Set<StringBuilder> dictionaryWords = new HashSet<>();
+    public static Map<Integer, StringBuilder> getDictionaryWords(String name) throws FileNotFoundException {
+        Map<Integer, StringBuilder> dictionaryWords = new HashMap<>();
 
         File file = new File(name);
         Scanner scanner = new Scanner(file);
 
+        int index = 0;
         while(scanner.hasNext()) {
             StringBuilder word = new StringBuilder(scanner.next());
-            dictionaryWords.add(word);
+            dictionaryWords.put(index, word);
+            index++;
         }
         return dictionaryWords;
     }
